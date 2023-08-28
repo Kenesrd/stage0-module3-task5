7b898789eb7c4708ab8c69ce4067aa51950a6015
kenes <kenesvip@gmail.com>
2023-08-28 19:45:30 +0500

    Module 3 Task 5 Done

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.task5;
 
+import java.math.BigDecimal;
+
 public class NumberDivider {
     void divide(int toBeDivided) {
+        float result = toBeDivided / 5f;
+        System.out.println(result);
     }
 }
